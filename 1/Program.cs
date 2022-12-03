cdc8989d4d949659ecae9ec3a434535add3bb455
Lera <miss_val@bk.ru>
2022-12-03 19:13:04 +0300

    Задача 1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Ваше число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 10000 || n > 99999)
+{
+    Console.Write("Введите 5-значное число: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+int i = 0;
+int m = n;
+while (m > 0)
+{
+    int q = m % 10;
+    i = i * 10 + q;
+    m = (m - q) / 10;
+}
+if (n == i)
+{
+    Console.Write("Это палиндром");
+} 
+else
+    Console.Write("Это не палиндром");
